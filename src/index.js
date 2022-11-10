4036de81e8edd0762a7d5c36912ad9c59f1371a2
Chris Mittell <mittell@hotmail.co.uk>
2022-11-10 15:47:14 +0900

    feat: added custom Tailwind setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './styles/app.css';
 import FirebaseContext from './context/firebase';
 import { firebase, FieldValue } from './lib/firebase';
 
