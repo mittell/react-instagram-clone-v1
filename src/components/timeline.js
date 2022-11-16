ab06e125c049c36a562f7e3cb596ef37e0959d8c
Chris Mittell <mittell@hotmail.co.uk>
2022-11-16 14:39:17 +0900

    feat: updated Timeline component

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,3 +1,19 @@
+import Skeleton from 'react-loading-skeleton';
+import usePhotos from '../hooks/use-photos';
+import Post from './post';
+
 export default function Timeline() {
-	return <div className='container col-span-2'>Timeline</div>;
+	const { photos } = usePhotos();
+
+	return (
+		<div className='container col-span-2'>
+			{!photos ? (
+				<Skeleton count={1} width={640} height={500} className='mb-5' />
+			) : photos?.length > 0 ? (
+				photos.map((content) => <Post key={content.docId} content={content} />)
+			) : (
+				<p className='text-center text-2xl'>Follow people to see photos!</p>
+			)}
+		</div>
+	);
 }
