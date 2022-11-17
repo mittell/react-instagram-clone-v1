4d225ce3b7f3dcae3fb8320132f60fccf3cca22a
Chris Mittell <mittell@hotmail.co.uk>
2022-11-17 16:56:45 +0900

    feat: added initial Profile page

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,3 +1,36 @@
+import { useState, useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import { getUserByUsername } from '../services/firebase';
+import * as ROUTES from '../constants/routes';
+
+import UserProfile from '../components/profile';
+import Header from '../components/header';
+
 export default function Profile() {
-	return null;
+	const { username } = useParams();
+	const [user, setUser] = useState(null);
+	const history = useHistory();
+
+	useEffect(() => {
+		async function checkUserExists() {
+			const userData = await getUserByUsername(username);
+
+			if (userData.length > 0) {
+				setUser(userData[0]);
+			} else {
+				history.push(ROUTES.NOT_FOUND);
+			}
+		}
+
+		checkUserExists();
+	}, [username, history]);
+
+	return user?.username ? (
+		<div className='bg-gray-background'>
+			<Header />
+			<div className='mx-auto max-w-screen-lg'>
+				<UserProfile user={user} />
+			</div>
+		</div>
+	) : null;
 }
