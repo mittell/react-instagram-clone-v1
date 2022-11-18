a17d14a74950b95f4a6a866bef6968bbaccb7070
Chris Mittell <mittell@hotmail.co.uk>
2022-11-18 13:35:05 +0900

    feat: added Profile Header logic and styling

--- src/components/profile/header.js
+++ src/components/profile/header.js
@@ -2,17 +2,110 @@ import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Skeleton from 'react-loading-skeleton';
 import useUser from '../../hooks/use-user';
+import { isUserFollowingProfile, toggleFollow } from '../../services/firebase';
 
 export default function Header({
 	photosCount,
-	profile,
+	profile: {
+		docId: profileDocId,
+		userId: profileUserId,
+		fullName,
+		following = [],
+		followers = [],
+		username: profileUsername,
+	},
 	followerCount,
 	setFollowerCount,
+	loggedInUserName,
 }) {
 	const { user } = useUser();
 	const [isFollowingProfile, setIsFollowingProfile] = useState(false);
+	const activeBtnFollow = user.username && user.username !== profileUsername;
 
-	return null;
+	const handleToggleFollow = async () => {
+		setIsFollowingProfile((isFollowingProfile) => !isFollowingProfile);
+		setFollowerCount({
+			followerCount: isFollowingProfile ? followerCount - 1 : followerCount + 1,
+		});
+
+		await toggleFollow(
+			isFollowingProfile,
+			user.docId,
+			profileDocId,
+			profileUserId,
+			user.userId
+		);
+	};
+
+	useEffect(() => {
+		const isLoggedInUserFollowingProfile = async () => {
+			const isFollowing = await isUserFollowingProfile(
+				user.username,
+				profileUserId
+			);
+			setIsFollowingProfile(!!isFollowing);
+		};
+
+		if (user.username && profileUserId) {
+			isLoggedInUserFollowingProfile();
+		}
+	}, [user.username, profileUserId]);
+
+	return (
+		<div className='grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg'>
+			<div className='container flex justify-center'>
+				{user.username && (
+					<img
+						className='rounded-full h-40 w-40 flex'
+						alt={`${profileUsername} profile`}
+						src={`/images/avatars/${profileUsername}.jpg`}
+					/>
+				)}
+			</div>
+			<div className='flex items-center justify-center flex-col col-span-2'>
+				<div className='container flex items-center'>
+					<p className='text-2xl mr-4'>{profileUsername}</p>
+					{activeBtnFollow && (
+						<button
+							className='bg-blue-medium font-bold text-sm text-white rounded w-20 h-8'
+							type='button'
+							onClick={handleToggleFollow}
+							onKeyDown={(event) => {
+								if (event.key === 'Enter') {
+									handleToggleFollow();
+								}
+							}}
+						>
+							{isFollowingProfile ? 'Unfollow' : 'Follow'}
+						</button>
+					)}
+				</div>
+				<div className='container flex mt-4'>
+					{followers === undefined || following === undefined ? (
+						<Skeleton count={1} width={677} height={24} />
+					) : (
+						<>
+							<p className='mr-10'>
+								<span className='font-bold'>{photosCount}</span> Photos
+							</p>
+							<p className='mr-10'>
+								<span className='font-bold'>{followerCount}</span>{' '}
+								{followerCount === 1 ? 'Follower' : 'Followers'}
+							</p>
+							<p className='mr-10'>
+								<span className='font-bold'>{following.length}</span> Following
+							</p>
+						</>
+					)}
+				</div>
+				<div className='container mt-4'>
+					<p className='font-medium'>
+						{!fullName ? <Skeleton count={1} height={24} /> : fullName}
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 Header.propTypes = {
@@ -24,5 +117,7 @@ Header.propTypes = {
 		userId: PropTypes.string,
 		fullName: PropTypes.string,
 		following: PropTypes.array,
+		followers: PropTypes.array,
+		username: PropTypes.string,
 	}).isRequired,
 };
