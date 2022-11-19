ee8c0cf8325ad240ef1c296e058ffd15206dadfd
Chris Mittell <mittell@hotmail.co.uk>
2022-11-19 16:01:46 +0900

    refactor: user context usage

--- src/components/profile/header.js
+++ src/components/profile/header.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Skeleton from 'react-loading-skeleton';
 import useUser from '../../hooks/use-user';
 import { isUserFollowingProfile, toggleFollow } from '../../services/firebase';
+import UserContext from '../../context/user';
 
 export default function Header({
 	photosCount,
@@ -10,17 +11,17 @@ export default function Header({
 		docId: profileDocId,
 		userId: profileUserId,
 		fullName,
-		following = [],
-		followers = [],
+		following,
+		followers,
 		username: profileUsername,
 	},
 	followerCount,
 	setFollowerCount,
-	loggedInUserName,
 }) {
-	const { user } = useUser();
+	const { user: loggedInUser } = useUser(UserContext);
+	const { user } = useUser(loggedInUser?.uid);
 	const [isFollowingProfile, setIsFollowingProfile] = useState(false);
-	const activeBtnFollow = user.username && user.username !== profileUsername;
+	const activeBtnFollow = user?.username && user?.username !== profileUsername;
 
 	const handleToggleFollow = async () => {
 		setIsFollowingProfile((isFollowingProfile) => !isFollowingProfile);
@@ -46,19 +47,25 @@ export default function Header({
 			setIsFollowingProfile(!!isFollowing);
 		};
 
-		if (user.username && profileUserId) {
+		if (user?.username && profileUserId) {
 			isLoggedInUserFollowingProfile();
 		}
-	}, [user.username, profileUserId]);
+	}, [user?.username, profileUserId]);
 
 	return (
 		<div className='grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg'>
-			<div className='container flex justify-center'>
-				{user.username && (
+			<div className='container flex justify-center items-center'>
+				{profileUsername ? (
 					<img
 						className='rounded-full h-40 w-40 flex'
 						alt={`${profileUsername} profile`}
 						src={`/images/avatars/${profileUsername}.jpg`}
+					/>
+				) : (
+					<img
+						className='rounded-full h-40 w-40 flex'
+						alt={`default profile`}
+						src={`/images/avatars/default.png`}
 					/>
 				)}
 			</div>
@@ -81,7 +88,7 @@ export default function Header({
 					)}
 				</div>
 				<div className='container flex mt-4'>
-					{followers === undefined || following === undefined ? (
+					{!followers || !following ? (
 						<Skeleton count={1} width={677} height={24} />
 					) : (
 						<>
--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,11 +1,11 @@
-import useUser from '../../hooks/use-user';
 import User from './user';
 import Suggestions from './suggestions';
+import { useContext } from 'react';
+import LoggedInUserContext from '../../context/logged-in-user';
 
 export default function Sidebar() {
-	const {
-		user: { docId, fullName, username, userId, following },
-	} = useUser();
+	const { user: { docId = '', fullName, username, userId, following } = {} } =
+		useContext(LoggedInUserContext);
 
 	return (
 		<div className='p-4'>
