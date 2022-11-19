5c1ff9797684f58bb8c52a160a6eef94a3c3311f
Chris Mittell <mittell@hotmail.co.uk>
2022-11-19 15:19:06 +0900

    feat: added loggedInUser Context

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,20 +1,32 @@
 import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import useUser from '../hooks/use-user';
+import LoggedInUserContext from '../context/logged-in-user';
+
 import Header from '../components/header';
 import Timeline from '../components/timeline';
 import Sidebar from '../components/sidebar';
 
-export default function Dashboard() {
+export default function Dashboard({ user: loggedInUser }) {
+	const { user } = useUser(loggedInUser.uid);
+
 	useEffect(() => {
 		document.title = 'Instasnap';
 	}, []);
 
 	return (
-		<div className='bg-gray-background'>
-			<Header />
-			<div className='grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg'>
-				<Timeline />
-				<Sidebar />
+		<LoggedInUserContext.Provider value={{ user }}>
+			<div className='bg-gray-background'>
+				<Header />
+				<div className='grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg'>
+					<Timeline />
+					<Sidebar />
+				</div>
 			</div>
-		</div>
+		</LoggedInUserContext.Provider>
 	);
 }
+
+Dashboard.propTypes = {
+	user: PropTypes.object.isRequired,
+};
