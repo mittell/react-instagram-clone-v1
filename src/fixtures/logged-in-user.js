bcd1ce5d8e0cd21c1d502615fc5958865f941cd5
Chris Mittell <mittell@hotmail.co.uk>
2022-12-08 10:40:38 +0900

    chore: added fixture

---
+++ src/fixtures/logged-in-user.js
@@ -0,0 +1,9 @@
+export default {
+	userId: 'vUqzGYXQ5xVCOaSWKctImtyiC5v1',
+	username: 'user',
+	fullName: 'User',
+	emailAddress: 'user@email.com',
+	following: ['2'],
+	followers: ['2', '3', '4'],
+	dateCreated: Date.now(),
+};
