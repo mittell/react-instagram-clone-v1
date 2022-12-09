1610a839cc251810b09d9cc1038328116893b6a5
Chris Mittell <mittell@hotmail.co.uk>
2022-12-09 12:49:17 +0900

    fix: removed unused var

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from 'react';
-import { useHistory, Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 import * as ROUTES from '../constants/routes';
 
