17847cecddfa5f1e97710d4048a862ac08fcd9f4
Chris Mittell <mittell@hotmail.co.uk>
2022-12-09 11:49:43 +0900

    feat: added fixtures

---
+++ src/fixtures/profile-followed-by-logged-in-user.js
@@ -0,0 +1,9 @@
+export default {
+	userId: '3',
+	username: 'orwell',
+	fullName: 'George Orwell',
+	emailAddress: 'george@donotfollow.com',
+	following: ['2'],
+	followers: ['vUqzGYXQ5xVCOaSWKctImtyiC5v1'],
+	dateCreated: 1613097964107,
+};
